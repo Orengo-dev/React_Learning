2f51385787e1ed273970022de56f7403a5b56e35
Orengo-dev <felix.orengo2000@gmail.com>
2024-06-19 22:16:43 +0300

    Initial commit with simple React page

---
+++ my-simple-react-page/src/App.js
@@ -0,0 +1,25 @@
+// src/App.js
+
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Simple React Page</h1>
+        <p>This is a demo - Running on my Local Machine</p>
+        <a
+          // className="App-link"
+          // href="https://reactjs.org"
+          // target="_blank"
+          // rel="noopener noreferrer"
+        >
+          Felix Orengo
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
